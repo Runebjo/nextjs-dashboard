a9b82cb91402aca2e3da6f46d92453bde6615a84
Runebjo <rune.bjorsnos@gmail.com>
2024-07-31 16:30:17 +0200

    Finished chapter 16 - adding metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next/dist/types";
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+
 export default function CustomersPage() {
     return <p>Customers Page</p>;
 }
